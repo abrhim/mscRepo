c09388509f76319d5b0e23ead5829951ff336b22
Brad Garropy <bradgarropy@gmail.com>
2021-09-01 11:33:07 -0500

    importing msc properly.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import "./styles.css";
 import React from "react";
 import mse from "@adobe/magento-storefront-events-sdk";
 import mse2 from "@adobe/magento-storefront-events-sdk";
-import msc from "@adobe/magento-storefront-event-collector";
 
 export default function App() {
-  msc;
   React.useEffect(() => {
+    // add mse to window
     window.magentoStorefrontEvents = mse;
-    console.log("init");
+
+    // msc must be initialized after mse is on window
+    import("@adobe/magento-storefront-event-collector");
+
+    // cannot reproduce duplicate contexts
     mse.context.setShopper({ shopperId: "logged-in" });
     mse.publish.pageView();
     mse2.context.setShopper({ shopperId: "logged-in" });
     mse2.publish.pageView();
   }, []);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
