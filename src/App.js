b004491154054f68bee50d5e8aea3a6f0dad6fd8
Abram Himmer <abramhimmer@gmail.com>
2021-09-01 10:09:19 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import React from "react";
+import mse from "@adobe/magento-storefront-events-sdk";
+import msc from "@adobe/magento-storefront-event-collector";
+
+export default function App() {
+  msc;
+  React.useEffect(() => {
+    window.magentoStorefrontEvents = mse;
+    console.log("init");
+    mse.context.setShopper({ shopperId: "logged-in" });
+    mse.publish.pageView();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
